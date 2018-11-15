75704c276f6bcdaf2a38fad13e1dd35db4de2ede
techcul_iOS <techcul_iOS@taikuios-liuxudongdeimac.local>
2018-11-15 16:46:36 +0800

    Initial Commit

---
+++ OCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCDemo
+//
+//  Created by techcul_iOS on 2018/11/15.
+//  Copyright © 2018 techcul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCDemo
+//
+//  Created by techcul_iOS on 2018/11/15.
+//  Copyright © 2018 techcul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
